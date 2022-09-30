5124101c07dfdae77e4430a924bacfadae5a266a
MattZKruspe <matthewmarquez2003@gmail.com>
2022-09-29 21:07:05 -0500

    Actualización Proyecto Carrito

--- js/app.js
+++ js/app.js
@@ -5,15 +5,37 @@ const listaCursos = documents.requerySelector('#lista-cursos');
 const vaciarCarrito = document.querySelector('#vaciar-Carrito');
 
 cargarEventListener();
-function cargarEventListener(){
-listaCursos.addEventListener('click', agregarCurso);
-
+function cargarEventListener() {
+    listaCursos.addEventListener('click', agregarCurso);
 }
 //Estas son las funciones
 function agregarCurso(e){
     e.preventDeFault();
     if(e.targer.classList.contains('agregar-carrito')){
-        console.log(e.target);
+        console.log(e.target.parentElement.parentElement);
+        leerDatosCurso(cursoSeleccionado);
+
     }
-    
 }
+/*-----------------------------------------------------------*/
+function leerDatosCurso(curso){
+    console.log(curso);
+}
+//cracion del objeto del curso actual
+const curso = {
+ imagen: curso.querySelector('img').src,
+ titulo: curso.querySelector('h4').textContent,
+ precio: curso.querySelector('.precio spam').textContent,
+ id: curso.querySelector('a').getAtribute('data-id'),
+ cantidad: 1
+}
+
+articulosCarrito.forEach(curso => {
+    const row =  document.createElement('tr');
+    row.innerHTML = ´
+        <td>
+            ${curso.titulo}
+        </td>
+        ´;
+});
+
